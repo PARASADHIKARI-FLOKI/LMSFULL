55a7d9fbb20a9ed52fbf118a2bfa7b88ac1b03e0
PARASADHIKARI-FLOKI <adparas1@gmail.com>
2025-04-13 11:40:58 +0545

    everyday

--- server/src/controllers/webhooks.js
+++ server/src/controllers/webhooks.js
@@ -3,10 +3,11 @@ import User from "../models/User.js"
 
 //API controller function to manage Clerk User with Database
 
-export const clerkwebhooks = async (requestAnimationFrame,res)=>{
+export const clerkwebhooks = async (req,res)=>{
     try {
         const whook =new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        await whook.verify(JSON.stringify(requestAnimationFrame.body),{
+        
+        await whook.verify(JSON.stringify(req.body),{
             "svix-id":req.headers['svix-id'],
             "svix-timestamp":req.headers["svix-timestamp"],
             "svix-signature":req.headers["svix-signature"]
